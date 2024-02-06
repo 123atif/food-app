ca880faab54160b8d74bcbd4ca4ca128ce1cb84c
Mohammad Atif Arshad <81535521+123atif@users.noreply.github.com>
2024-02-06 16:36:51 +0500

    Here i add new component with Horizon-- name to add a line

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Horizontalline from './components/Horizontalline';
 
 function App() {
   return (
      <div className="h-screen bg-stone-950">
-     
       <Navbar />
+      <Horizontalline/>
     </div>
   );
 }
---
+++ src/components/Horizontalline.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Horizontalline = () => {
+  return (
+    <div>
+       <hr className="my-1 border-2 border-yellow-500"/>
+    </div>
+  )
+}
+
+export default Horizontalline;
