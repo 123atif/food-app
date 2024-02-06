1b6e04463d6fc9948586b98d651fc92f73966626
Mohammad Atif Arshad <81535521+123atif@users.noreply.github.com>
2024-02-06 13:01:27 +0500

    add icons & add images & init navbar

--- Component/Navbar.jsx
+++ Component/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { BsSearch } from "react-icons/bs";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
-  )
-}
+    <div className='flex justify-between items-center'>
+    <img src='../src/images/logo.png' style={{ height: '100px', width: '110px' }}/>
+    <ul className="text-white flex">
+        <li  className='px-2'>Home</li>
+        <li className='px-2'>about</li>
+        <li className='px-2'>menu</li>
+        <li className='px-2'>products</li>
+        <li className='px-2'>review</li>
+        <li className='px-2'>contact</li>
+        <li className='px-2'>blogs</li>
+        <li className='px-2'>cart</li>
+      </ul>
+      <div>
 
-export default Navbar
+      <BsSearch className="text-white"/>
+      <FaShoppingCart className="text-white"/>
+      </div>
+    </div>
+    
+  );
+};
+
+export default Navbar;
--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from '../Component/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-      <Routes>
-        <Route index="/" element={<home/>}>
-          <Route path="about" element={<About />}/>
-          <Route path="menu" element={<Menu/>}/>
-          <Route path="products" element={<Products/>}/>
-          <Route path="reviews" element={<Reviews/>}/>
-          <Route path="contact" element={<Contact/>}/>
-          <Route path="blogs" element={<Blogs/>}/>
-          <Route path="cart" element={<Cart/>}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-      <Navbar />
+     <div className="h-screen bg-stone-950	">
+     
+      <Navbar/>
     </div>
   );
 }
