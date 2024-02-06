21aa8c4b8da376b58e0ef1bff2bac64a56ecf424
Mohammad Atif Arshad <81535521+123atif@users.noreply.github.com>
2024-02-06 14:20:59 +0500

    Add images folder & Add icon npm in navbar component & work on navbar

--- Component/Navbar.jsx
+++ Component/Navbar.jsx
@@ -7,15 +7,15 @@ const Navbar = () => {
     <div className='flex justify-between items-center'>
     <img src='../src/images/logo.png' style={{ height: '100px', width: '110px' }}/>
     <ul className="text-white flex">
-        <li  className='px-2'>Home</li>
-        <li className='px-2'>about</li>
-        <li className='px-2'>menu</li>
-        <li className='px-2'>products</li>
-        <li className='px-2'>review</li>
-        <li className='px-2'>contact</li>
-        <li className='px-2'>blogs</li>
-        <li className='px-2'>cart</li>
-      </ul>
+        <li>Home</li>
+        <li>about</li>
+        <li>menu</li>
+        <li>products</li>
+        <li>review</li>
+        <li>contact</li>
+        <li>blogs</li>
+        <li>cart</li>
+      </ul> 
       <div>
 
       <BsSearch className="text-white"/>
